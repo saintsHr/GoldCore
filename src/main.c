96beb416f5e306af5dc71f03dcc484631290e796
saintsHr <hiqkrs@gmail.com>
2025-11-28 14:21:17 -0300

    a

--- src/main.c
+++ src/main.c
@@ -7,26 +7,11 @@ int main(){
     gld_windowSetContext(window);
     gld_windowSetup(window, GLD_2D);
 
-    int track = 0;
-
     while (!gld_windowShouldClose(window)){
         gld_windowClear();
 
-        glRotatef(1, 0, track, 0);
 
-        glBegin(GL_TRIANGLES);
-            glColor3f(1.0f, 0.0f, 0.0f);
-            glVertex2f(-0.5f, -0.4f);
 
-            glColor3f(0.0f, 1.0f, 0.0f);
-            glVertex2f(0.0f, 0.6f);
-
-            glColor3f(0.0f, 0.0f, 1.0f);
-            glVertex2f(0.5f, -0.4f);
-        glEnd();
-
-        track++;
-        
         gld_windowHandle(window);
     }
 
